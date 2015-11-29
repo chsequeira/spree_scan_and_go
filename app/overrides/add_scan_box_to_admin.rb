bf79fbe5a938b04761e6c9ee0a194c3b2307bc83
Christian <ch.sequeira@chweb.pt>
2015-11-29 19:50:02 +0000

    Scan box adjustments

--- app/overrides/add_scan_box_to_admin.rb
+++ app/overrides/add_scan_box_to_admin.rb
@@ -2,4 +2,6 @@ Deface::Override.new(
   :virtual_path   => "spree/layouts/admin",
   :insert_bottom  => "#wrapper",
   :name           => "add_scan_box_to_admin",
-  :partial        => "spree/admin/scan_and_go/scan_box")
+  :partial        => "spree/admin/scan_and_go/scan_box",
+  :original       => '4d78002a579caedd16f969f1f91fd1b61c4ee428'
+)
